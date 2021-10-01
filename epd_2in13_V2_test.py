a545156288de0865dc7885cc542b59051a1744b3
gef@pi <gef.faelden@gmail.com>
2021-10-01 21:40:29 +0800

    changes from pi

--- epd_2in13_V2_test.py
+++ epd_2in13_V2_test.py
@@ -3,7 +3,7 @@
 import sys
 import os
 picdir = '/pic'
-libdir = '/lib'
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
@@ -22,19 +22,19 @@ try:
     logging.info("init and Clear")
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
-    
+   
     # read bmp file 
     logging.info("1.read bmp file...")
-    image = Image.open(os.path.join(picdir, 'test.bmp'))
+    image = Image.new('1', (epd.height, epd.width), 255) 
+    bmp = Image.open('test.bmp')
+    image.paste(bmp, (2,2))
     epd.display(epd.getbuffer(image))
     time.sleep(10)
-
+    
     # epd.Clear(0xFF)
     logging.info("Clear...")
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
-    
-    logging.info("Goto Sleep...")
     epd.sleep()
         
 except IOError as e:
